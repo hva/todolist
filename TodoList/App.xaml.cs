4e6ed3a7c91ffb199d5075ef39ef92e98266607f
Yauheni Khvaliuk <hvaluk@gmail.com>
2015-11-28 17:46:37 +0100

    initial commit

---
+++ TodoList/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Prism.Unity.Windows;
+
+namespace TodoList
+{
+    sealed partial class App : PrismUnityApplication
+    {
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            return Task.FromResult<object>(null);
+        }
+    }
+}
