77e87c8c73135132e8dffda88495dd6b894f0359
Yauheni Khvaliuk <hvaluk@gmail.com>
2015-12-02 23:45:52 +0100

    hide status bar for mobile

--- TodoList.UWP/Views/MainPage.xaml.cs
+++ TodoList.UWP/Views/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.Foundation.Metadata;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
 
 namespace TodoList.UWP.Views
 {
@@ -7,6 +10,16 @@ namespace TodoList.UWP.Views
         public MainPage()
         {
             InitializeComponent();
+
+            HideStatusBar();
+        }
+
+        private static async void HideStatusBar()
+        {
+            if (ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar"))
+            {
+                await StatusBar.GetForCurrentView().HideAsync();
+            }
         }
     }
 }
